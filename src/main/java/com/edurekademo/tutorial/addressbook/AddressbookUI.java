e895e31929a418e13466b48debcd370f35f415b2
saikatkonar <33577844+saikatkonar@users.noreply.github.com>
2017-12-20 10:40:42 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Testing for RelQSoft DevOPs training");
+        filter.setInputPrompt("Testing for RelQSoft DevOPs training adding to this");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
